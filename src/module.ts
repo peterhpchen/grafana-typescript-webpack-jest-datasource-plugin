574e224e68e8441a171ad25ab1c2952b17508957
Peter Chen <peter3598768@gmail.com>
2018-08-08 18:02:49 +0800

    feature: add metadata and entry point

---
+++ src/module.ts
@@ -0,0 +1,9 @@
+class DemoDatasource {}
+class DemoQueryCtrl {}
+class DemoConfigCtrl {}
+
+export {
+    DemoDatasource as Datasource,
+    DemoQueryCtrl as QueryCtrl,
+    DemoConfigCtrl as ConfigCtrl,
+};
